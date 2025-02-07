2514b2ba9849e47f98db258f8f63005756f5f519
Moorvi Garg <moorvi0659.be23@chitkara.edu.in>
2025-02-07 15:02:27 +0530

    done

---
+++ 3/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) =>{
+    //console.log(req);
+    //console.log(res);
+    console.log('Time',Date.now());
+    next();
+})
+
+app.get('/',(req,res) => {
+    res.send('Hello World');
+})
+
+app.listen(port,()=>{
+    console.log('Server is running on http://localhost:${port}');
+});
